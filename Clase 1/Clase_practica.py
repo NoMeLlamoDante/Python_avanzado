aa67c776099eb13ba50fc9e5839cda764aa0a2d1
dante <eo.zm24@outlook.com>
2022-08-15 21:45:45 -0500

    commit inicial

---
+++ Clase 1/Clase_practica.py
@@ -0,0 +1,9 @@
+#Crear clase
+class MiPrimerClase:
+    pass
+
+#Instancia
+primer_objeto = MiPrimerClase()
+
+#Comprobar que el objeto se crea
+print(primer_objeto)
---
+++ Clase 1/casa.py
@@ -0,0 +1,7 @@
+class Casa:
+    def __init__(self):
+        pass
+
+casita = Casa()
+
+print(casita);
